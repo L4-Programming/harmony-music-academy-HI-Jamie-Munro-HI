d62b7d187056550626186674998460c01f3e0b00
HI-Jamie-Munro-HI <1841793@hc.ac.je>
2024-11-07 11:09:32 +0000

    Added comment plan for form logic

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// javascript
-
-console.log("You are signing up");
+// Display a form to the user = make sure it loads correctly
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+// Get the user's email address = userEmail (string)
+// Get the user's level = userLevel (string)
+// Get the user's hours of study = userHours (number)
+// Validate the user's input
+// Check if the user has selected a level = make sure that the level chosen decides the hours allowed
+// Check if the user has provided an email address = make sure the email is valid
+// Check if the user has specified at least one hour of study = must be => 1
+// Check if the number of hours requested is within the allowed range = compare to the user's selected level
+// Calculate the total cost = userHours * levelChosenCost
+// Display the total cost to the user = make sure its on the same page
